8639a60374928967d8770b1939d94b8091b6bd52
Draigo231 <140915715+Draigo231@users.noreply.github.com>
2024-08-22 23:34:40 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,7 @@ public class Main {
     public static void main(String[] args) {
         BonusMilesService service = new BonusMilesService();
         int price = 10_000;
-        int bonusDivider = 20;
-        int miles = service.calculate(price, bonusDivider); // должно получиться 500
+        int miles = service.calculate(price); // должно получиться 500
         System.out.println("Общая стоимость билета: " + price);
         System.out.println("Сумма начисленных бонусов: " + miles);
     }
